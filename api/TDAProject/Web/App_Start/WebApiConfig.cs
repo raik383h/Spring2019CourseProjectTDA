64bbed7e63e9a78395f8690d17e2a6db481d3f1d
Matthew Meacham <mmeacham6@gmail.com>
2019-01-02 16:04:37 -0600

    Enable CORS for local Angular App and switch to JSON for default serialization

--- api/TDAProject/Web/App_Start/WebApiConfig.cs
+++ api/TDAProject/Web/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
 
 namespace Web
 {
@@ -19,6 +21,14 @@ namespace Web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Enable CORS for the Angular App
+            // NOTE: Since we are running both the frontend and the backend locally, we can trust this origin with all headers and methods (the *). In practice, we would only allow really needed headers and methods
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
+
+            // Make JSON the default formatter over XML
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
